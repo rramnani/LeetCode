7ec31b9359dc1dcb7418fe53cd3526f38adfa408
rramnani <36605356+rramnani@users.noreply.github.com>
2022-05-11 13:47:54 -0500

    Update validStartingCity.py

--- validStartingCity.py
+++ validStartingCity.py
@@ -13,3 +13,27 @@ def validStartingCity(distances, fuel, mpg):
 			return startingCityIdx
 
     return -1
+
+
+# O(N) time, O(1) space
+
+class Solution:
+    def validStartingCity(self, distances, fuel, mpg):
+
+        numberOfCities = len(distances)
+        milesRemaining = 0
+        indexOfStartingCityCandidate = 0
+        milesRemainingAtStartingCityCandidate = 0
+
+        for cityIdx in range(1, numberOfCities):
+            distanceFromPreviousCity = distances[cityIdx - 1]
+            fuelFromPreviousCity = fuel[cityIdx - 1]
+
+            milesRemaining += fuelFromPreviousCity * mpg - distanceFromPreviousCity
+
+            if milesRemaining < milesRemainingAtStartingCityCandidate:
+                indexOfStartingCityCandidate = cityIdx
+                milesRemainingAtStartingCityCandidate = milesRemaining
+                
+
+        return indexOfStartingCityCandidate
