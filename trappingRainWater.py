fd2c8dba014c7d29621fdaf2c9656730bd832645
rramnani <36605356+rramnani@users.noreply.github.com>
2022-05-25 07:38:39 -0500

    Update trappingRainWater.py
    Added O(N) solution

--- trappingRainWater.py
+++ trappingRainWater.py
@@ -10,3 +10,29 @@ class Solution(object):
             right = 0 if i == len(height) - 1 else max(height[i + 1:])
             res += max(0, min(left, right) - val)
         return res
+
+ # O(N) time | O(1) space
+ class Solution:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        res = 0
+        maxLeft, maxRight = height[left], height[right]
+
+        while left < right:
+            if maxLeft <= maxRight:
+                left += 1
+                maxLeft = max(maxLeft, height[left])
+                print("maxLeft = ", maxLeft)
+                res += maxLeft - height[left]
+                print("I'm bounded by left = ", res)
+            else:
+                right -= 1
+                maxRight = max(maxRight, height[right])
+                print("maxRight = ", maxRight)
+                res += maxRight - height[right]
+
+        return res
