460c46022be9a6633c882e034efeb2a6f6b64137
rramnani <36605356+rramnani@users.noreply.github.com>
2022-06-07 11:31:32 -0500

    Update numIslands.py
    2nd iteration for DFS

--- numIslands.py
+++ numIslands.py
@@ -25,3 +25,25 @@ def numIslands(matrix):
                 dfs(r,c)
                 count+=1
     return (count)
+
+
+# DFS O(M*N) time | O(1) space
+
+class Solution(object):
+    def numIslands(self, grid):
+        islands = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    self.dfs(r, c, grid)
+                    islands += 1
+        return islands 
+    
+    def dfs(self, r, c, grid):
+        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != "1":
+            return
+        grid[r][c] = "0"
+        self.dfs(r, c + 1, grid)
+        self.dfs(r, c - 1, grid)
+        self.dfs(r - 1, c, grid)
+        self.dfs(r + 1, c, grid)
